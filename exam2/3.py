9aea16ab1234f22ff123df7ed3d9cf4d9e50f047
Shayan Shahbazi <upm200@gmail.com>
2025-09-21 14:46:26 +0330

    Update 3.py

--- exam2/3.py
+++ exam2/3.py
@@ -1,5 +1,5 @@
 num1, num2 = int(input()), int(input())
 
-if num1 < num2: print(num1 , ">", num2) 
-elif num2 < num1: print(num2, " > ", num1) 
-else: print(num1, " = ", num2)
+if num1 < num2: print(num1, ">", num2)
+elif num2 < num1: print(num2, ">", num1)
+else: print(num1, "=", num2)
